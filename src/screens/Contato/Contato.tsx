c8c5b0d6f74061b4dbea105c0e0c1c03a34111d2
Nathalia <natgorito@gmail.com>
2023-11-16 20:08:19 -0300

    MenuConfig

--- src/screens/Contato/Contato.tsx
+++ src/screens/Contato/Contato.tsx
@@ -6,16 +6,13 @@ import { Image } from "@gluestack-ui/themed";
 import { FontAwesome } from "@expo/vector-icons";
 import { NavigationProp } from "@react-navigation/native";
 
-export const MenuConfig = ({ navigation }: { navigation: NavigationProp<any> }) =>
+export const Contato = ({ navigation }: { navigation: NavigationProp<any> }) =>
 {
     return (
       <GluestackUIProvider>
-        <Header title="Configuração"></Header>
+        <Header title="Contato"></Header>
         <BoxContainer alignItems={"center"}>
-          <DButton text="SENHA MASTER" onPress={() => navigation.navigate("../../SenhaMaster/SenhaMaster.tsx")}/>
-          <DButton text="EXCLUIR CADASTRO" onPress={() => navigation.navigate("ExcluirCadastro")}/>
-          <DButton text="TERMOS" onPress={() => navigation.navigate("Termos/Termos")}/>
-          <DButton text="CONTATO" onPress={() => navigation.navigate("Contato")} />
+          
           <Box flexDirection="row" justifyContent="flex-start" alignItems="center" w={"95%"}>
             <FontAwesome name="chevron-left" size={28} color="black" onPress={() => navigation.navigate("Home")} />
           </Box>
---
+++ src/screens/SenhaMaster/SenhaMaster.tsx
@@ -0,0 +1,29 @@
+import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { Header } from "../../components/Header";
+import { DButton } from "../../components/DButton";
+import { BoxContainer } from "../../components/BoxContainer";
+import { DInput } from "../../components/DInput";
+import { Image } from "@gluestack-ui/themed";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+import { NavigationProp } from "@react-navigation/native";
+
+export const SenhaMaster = ({ navigation }: { navigation: NavigationProp<any>; }) =>
+{
+    const [senhaMaster, setSenhaMaster] = useState<string>("");
+  return (
+    <GluestackUIProvider>
+      <Header title="Senha Master"></Header>
+      <BoxContainer alignItems={"flex-start"}>
+      <Text fontSize="$md" fontWeight="$bold">Cadastre a Senha Master*</Text>
+        <DInput placeholder="Senha Master" type={"password"} width="$90%" onChange={setSenhaMaster}/>
+        <Text fontSize="$md" fontWeight="$bold">Repita a Senha Master*</Text>
+        <DInput placeholder="Senha Master" type={"password"} width="$90%" onChange={setSenhaMaster}/>
+        <Box flexDirection="row" justifyContent="flex-start" alignItems="center" w={"95%"}>
+            <FontAwesome name="chevron-left" size={28} color="black" onPress={() => navigation.navigate("Home")}/>
+            <FontAwesome name="check" size={32} color="green" onPress={casdastrarSenhaMaster}/>
+        </Box>
+      </BoxContainer>
+    </GluestackUIProvider>
+  );
+};
