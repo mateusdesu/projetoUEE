7a2e06d287c11594f1480c1b199df45206bceb92
Nathalia <natgorito@gmail.com>
2023-11-20 14:02:37 -0300

    Inserção das opções no dropdown de ExcluirCadastro

--- src/screens/ExcluirCadastro/index.tsx
+++ src/screens/ExcluirCadastro/index.tsx
@@ -8,17 +8,21 @@ import { FontAwesome } from "@expo/vector-icons";
 import { DSelect } from "../../components/DSelect";
 import { useState } from "react";
 import { NavigationProp } from "@react-navigation/native";
+import {View, StyleSheet} from 'react-native';
 import React from "react";
 
 export const ExcluirCadastro = ({ navigation }: { navigation: NavigationProp<any>; }) =>
 {
-    const [senhaMaster, setSenhaMaster] = useState<string>("");
+    const [exclusao, setExclusao] = useState([
+        {label: "Eleições", value: "1"},
+        {label: "Candidatos", value: "2"},]);
   return (
     <GluestackUIProvider>
-      <Header title="Senha Master"></Header>
+      <Header title="Excluir Cadastro"></Header>
       <BoxContainer alignItems={"flex-start"}>
       <Text fontSize="$md" fontWeight="$bold">Qual tipo de dado deseja excluir?*</Text>
-        <DSelect items={eleicao.map((option) => ({label: option.label, value: option.value,}))} onChangeValue={handleElectionChange} zIndex={10000}/>
+        <DSelect items={exclusao.map((option) => ({label:option.label, value:option.value}))}  zIndex={10000}/>
+  
         <Box flexDirection="row" justifyContent="flex-start" alignItems="center" w={"95%"}>
             <FontAwesome name="chevron-left" size={28} color="black" onPress={() => navigation.navigate("MenuConfig")}/>
             <FontAwesome name="check" size={32} color="green"/>
