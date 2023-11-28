c87a3e6eee08172fe7941960c41c8b77e618973c
Mateus <mateuslopes1717@gmail.com>
2023-11-28 14:11:49 -0300

    Menu config screens added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,18 +5,28 @@ import { CadastrarCandidato } from "../screens/CadastrarCandidato";
 import { RealizarEleicao } from "../screens/RealizarEleicao";
 import { ResultadoEleicao } from "../screens/ResultadoEleicao";
 import { MenuConfig } from "../screens/MenuConfig";
+import { SenhaMaster } from "../screens/SenhaMaster";
+import { ExcluirCadastro } from "../screens/ExcluirCadastro";
+import { Termos } from "../screens/Termos";
+import { Contato } from "../screens/Contato";
 
 const Stack = createNativeStackNavigator();
 
 export const Routes = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: false, }}>
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, gestureEnabled: false }}
+    >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="CadastrarEleicao" component={CadastrarEleicao} />
       <Stack.Screen name="CadastrarCandidato" component={CadastrarCandidato} />
       <Stack.Screen name="RealizarEleicao" component={RealizarEleicao} />
       <Stack.Screen name="ResultadoEleicao" component={ResultadoEleicao} />
       <Stack.Screen name="MenuConfig" component={MenuConfig} />
+      <Stack.Screen name="SenhaMaster" component={SenhaMaster} />
+      <Stack.Screen name="ExcluirCadastro" component={ExcluirCadastro} />
+      <Stack.Screen name="Termos" component={Termos} />
+      <Stack.Screen name="Contato" component={Contato} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/Termos/index.tsx
@@ -0,0 +1,31 @@
+import { Box, GluestackUIProvider } from "@gluestack-ui/themed";
+import { Header } from "../../components/Header";
+import { DButton } from "../../components/DButton";
+import { BoxContainer } from "../../components/BoxContainer";
+import { Image } from "@gluestack-ui/themed";
+import { FontAwesome } from "@expo/vector-icons";
+import { NavigationProp } from "@react-navigation/native";
+import React from "react";
+
+export function Termos({ navigation }: { navigation: NavigationProp<any> }) {
+  return (
+    <GluestackUIProvider>
+      <BoxContainer alignItems={"center"}>
+        <Header title="Termos"></Header>
+        <Box
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          w={"95%"}
+        >
+          <FontAwesome
+            name="chevron-left"
+            size={28}
+            color="black"
+            onPress={() => navigation.navigate("MenuConfig")}
+          />
+        </Box>
+      </BoxContainer>
+    </GluestackUIProvider>
+  );
+}
